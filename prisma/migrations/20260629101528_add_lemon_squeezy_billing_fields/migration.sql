60e1decf9af5963075c850a028b9f39f4eb00fc9
ivko <ivkovicd69@gmail.com>
2026-06-29 12:15:48 +0200

    Add Lemon Squeezy billing fields

---
+++ prisma/migrations/20260629101528_add_lemon_squeezy_billing_fields/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "lemonSqueezyCustomerId" TEXT,
+ADD COLUMN     "lemonSqueezyOrderId" TEXT,
+ADD COLUMN     "lemonSqueezySubscriptionId" TEXT,
+ADD COLUMN     "lemonSqueezyVariantId" TEXT;
